0c5956cda8a1adcf577368600d639dd920e4cc99
Nick Allen <rutherford.t.b@gmail.com>
2021-12-01 18:41:57 -0600

    day1part1 complete

---
+++ day-1/day1Part1.js
@@ -0,0 +1,17 @@
+//count the number of times a depth measurement increases 
+//from the previous measurement.
+
+let inputStr = require('./input.js');
+let input = inputStr.split('\n');
+
+function isDeeper(depthArr){
+    let deeper = 0;
+    for(let i = 1; i<input.length; i++){
+        if(Number(input[i]) > Number(input[i-1])){
+            deeper++;
+        }
+    }
+    return deeper;
+}
+
+console.log(isDeeper(inputStr));
