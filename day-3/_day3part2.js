304cde2951b24136798f1a8897d043cf55c54b9b
Nick Allen <rutherford.t.b@gmail.com>
2021-12-04 10:07:29 -0600

    day 3 part 2 completed and passing

--- day-3/_day3part2.js
+++ day-3/_day3part2.js
@@ -1,37 +1,41 @@
 const inputArr = require('./day3input.js');
 const testInputArr = require('./day3testInput.js');
 
-function calculateLifeSupportRating(inputArr) {
-    const n = inputArr.length;
-    let gamma = '';
-    let epsilon = '';
-
-    //Determine most common bit per position
-    // gamma prints 1 if equal (oxygen generator)
-    // epsilon prints 0 if equal (CO2 scrubber)
-    for (let i = 0; i < inputArr[0].length; i++) {
-        let countOnes = 0;
-        
-        for (let bin of inputArr) {
-            if(Number(bin[i])) countOnes++;
-        }
-
-        gamma += countOnes >= n - countOnes ? '1':'0';
-        epsilon += countOnes >= n - countOnes ? '0':'1';
-    }
-
-    // Use gamma and epsilon to determine life support filter
-    let oxyArr = new Array(n);
-    let i = 0;
-
-    while(oxyArr.length > 1){
-        oxyArr = oxyArr.filter(bin => bin[i] === gamma[i]);
-    }
-
-    console.log(oxyArr);
-
-    return parseInt(gamma, 2) * (parseInt(epsilon, 2));
-}
-
 console.log(calculateLifeSupportRating(testInputArr)) // 230 (23 * 10)
 console.log(calculateLifeSupportRating(inputArr));
+
+function calculateLifeSupportRating(inputArr) {
+    const n = inputArr.length;
+
+    let oxygen = getLifeSupportRating(true, inputArr);
+    let carbon = getLifeSupportRating(false, inputArr);
+
+    console.log(oxygen);
+    console.log(carbon);
+
+    return parseInt(oxygen, 2) * (parseInt(carbon, 2));
+}
+
+function getLifeSupportRating(isOxygen, arr){
+    let nums = [...arr];
+    let oxyOrCarb = isOxygen ? '1' : '0';
+
+    for(let i=0; i<nums[0].length; i++){
+        let count = 0;
+
+        for(let bin of nums){
+            if(bin[i] === oxyOrCarb) count++;
+        }
+
+        if(count > nums.length - count){
+            nums = nums.filter(bin => bin[i] === '1');
+        }else if(count < nums.length - count){
+            nums = nums.filter(bin => bin[i] === '0');
+        }else{
+            nums = nums.filter(bin => bin[i] === oxyOrCarb);
+        }
+
+        if(nums.length > 1) continue;
+        return nums[0];
+    }
+}
