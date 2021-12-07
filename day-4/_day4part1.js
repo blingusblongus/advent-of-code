c0b4cab08a8401d284b78b4e4dc10c0ca866444b
Nick Allen <rutherford.t.b@gmail.com>
2021-12-06 21:16:43 -0600

    day 4 part 1 complete

--- day-4/_day4part1.js
+++ day-4/_day4part1.js
@@ -1,32 +1,65 @@
-let testInput = require('./day3TestInput.js');
+let testInput = require('./day4TestInput.js');
+let input = require('./day4Input.js');
 
-function pickBingo(testInput){
-    let picks = testInput.picks.split(' ');
-    
+function pickBingo(testInput) {
+    let picks = testInput.picks.split(',');
+    console.log(picks);
+
     //parse the input into an array of boards
     let regex = /\s+/;
     let nums = testInput.boards.split(regex);
     let lines = [];
     let boardsArr = [];
 
-    let totalNums = Number(nums.length);
-    console.log(totalNums)
-    console.log(nums);
-
-    for(let i=0; 0<nums.length; i++){
-        let line = nums.splice(0,5);
+    // fill boardsArr
+    for (let i = 0; 0 < nums.length; i++) {
+        let line = nums.splice(0, 5);
         lines.push(line);
     }
 
-    console.log(lines);
-
-    for(let i=0; 0<lines.length/5; i++){
-        let board = lines.splice(0,5);
+    for (let i = 0; 0 < lines.length / 5; i++) {
+        let board = lines.splice(0, 5);
         boardsArr.push(board);
     }
 
-    console.log(picks);
-    console.log(boardsArr);
+    let best = {
+        boardIndex: null,
+        bestPickIndex: Infinity
+    }
+    // Check for wins
+    for (let i = 0; i < boardsArr.length; i++) {
+        let board = boardsArr[i];
+        //score horizontal rows
+        for (let j=0; j < 5; j++) {
+            let h = [];
+            let v = [];
+            for(let k=0; k<5; k++){
+                v.push(picks.indexOf(board[k][j]));
+                h.push(picks.indexOf(board[j][k]));
+            }
+
+            let maxH = Math.max(...h);
+            let maxV = Math.max(...v);
+            if(maxH < best.bestPickIndex) best = {boardIndex: i, bestPickIndex: maxH}
+            if(maxV < best.bestPickIndex) best = {boardIndex: i, bestPickIndex: maxV}
+        }
+    }
+
+    const lastPick = picks[best.bestPickIndex];
+    let flatBoard = [];
+    for (let line of boardsArr[best.boardIndex]){
+        flatBoard = [...flatBoard, ...line];
+    }
+    let selected = picks.splice(0, best.bestPickIndex + 1);
+    console.log('selected', selected);
+
+    let unmarked = flatBoard.filter(el => !selected.includes(el));
+    console.log('unmarked', unmarked);
+    let sumUnmarked = unmarked.reduce((sum, el) => sum += parseInt(el), 0);
+    console.log('sumUnmarked', sumUnmarked);
+
+    return sumUnmarked * lastPick;
 }
 
-pickBingo(testInput); // 188 * 24 = 4512.
+console.log(pickBingo(testInput)); // 188 * 24 = 4512.
+console.log(pickBingo(input));
