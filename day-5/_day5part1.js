902d8479b057da97fe5e58a5a27632ba8c2b1b79
Nick Allen <rutherford.t.b@gmail.com>
2021-12-07 20:59:18 -0600

    day 5 part 1 complete

--- day-5/_day5part1.js
+++ day-5/_day5part1.js
@@ -1,4 +1,5 @@
 const testInput = require('./day5testInput.js');
+const input = require('./day5input.js');
 
 function findVents(input){
     const regex = /\n/;
@@ -23,17 +24,37 @@ function findVents(input){
     //init map
     let map = [];
     for(let y = 0; y<=maxY; y++){
-        map.push(Array(maxX+1).map(el => 0));
+        map.push([]);
+        for(let x=0; x<=maxX; x++){
+            map[y][x] = 0;
+        }
     }
 
-    console.log(map[4]);
-    console.log(map);
+    for(let line of vents){
+        let x1 = line[0][0];
+        let x2 = line[1][0];
+        let y1 = line[0][1];
+        let y2 = line[1][1];
 
-    console.log(maxX, " " , maxY);
+        if(x1 === x2){
+            for(let y=Math.min(y1,y2); y<=Math.max(y1,y2); y++){
+                map[y][x1] += 1;
+            }
+        }else if (y1 === y2){
+            for(let x=Math.min(x1,x2); x<=Math.max(x1,x2); x++){
+                map[y1][x] += 1;
+            }
+        }else if(y2 - y1 === x2 - x1){
+            console.log('diag');
+            console.log(line);
+        }
+    }
 
-    console.log(vents);
+    // console.log(map);
 
-    return 'end func';
+
+    return map.flat().filter(el => el > 1).length;
 }
 
-console.log(findVents(testInput));
+console.log(findVents(testInput)); // 5
+console.log(findVents(input));
