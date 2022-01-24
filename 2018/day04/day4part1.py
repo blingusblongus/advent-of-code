112e86e5019489ebc8204f1fcf67660b29e88692
Nick Allen <rutherford.t.b@gmail.com>
2022-01-24 16:53:52 -0600

    2018 day4part1 added sorting and guardNums

--- 2018/day04/day4part1.py
+++ 2018/day04/day4part1.py
@@ -1,26 +1,55 @@
 import datetime
 import re
-from sqlite3 import Date
 
 fInput = open('2018/day04/input.txt', 'r')
 fTest = open('2018/day04/test.txt', 'r')
 
 def processSleepData(input):
     
+    entries = []
     for line in input:
-        print(parseSleepData(line))
+        entries += [parseSleepData(line)]
+
+    # sort entries by date
+    entries = sorted(
+        entries, key=lambda x: (
+            x['year'], 
+            x['month'],
+            x['day'],
+            x['minute']))
+    # print(entries)
+
+    guards = {}
+    curGuard = 0
+    sleep = 0
+    wake = 0
+    for entry in entries:
+        if 'guardNum' in entry:
+            curGuard = entry['guardNum']
+        if entry['status'] == 'falls':
+            sleep = entry['minutes']
+
+
 
 def parseSleepData(log):
     date = re.findall('\[(.*?) ', log)[0]
+    [year, month, day] = date.split('-');
     times = re.findall(' (.*):(.*)]', log)[0]
     status = re.findall('] (.*?) ', log)[0]
-    print(times)
 
-    return {
-        'date': datetime.date(date, '%Y-%b-%d'),
+    data = {
+        'year': int(year),
+        'month': int(month),
+        'day': int(day),
         'hours': int(times[0]),
         'minute': int(times[1]),
         'status': status
     }
 
+    if status == 'Guard':
+        data['guardNum'] = int(re.findall('#(.*?) ', log)[0])
+
+    return data
+
 print(processSleepData(fTest))
+# print(processSleepData(fInput))
