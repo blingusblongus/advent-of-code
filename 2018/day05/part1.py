1e555de6882159601ea06fd02f318295e88df1e4
Nick Allen <rutherford.t.b@gmail.com>
2022-02-18 14:36:01 -0600

    2018 day5 part 1 complete

--- 2018/day05/part1.py
+++ 2018/day05/part1.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 from input import fullInput
 
 # build dictionary of matching polymers
@@ -7,13 +8,28 @@ for i in range(26):
     matches[letter] = letter.upper()
     matches[letter.upper()] = letter
 
-
-def collapse_polymer(str):
+def collapse_polymer(string):
+    while True:
+        altered = False
+        for i in range(len(string) - 2):
+            print("length: ", len(string))
+            if string[i] == '\n' or string[i+1] == '\n':
+                continue
+            elif checkPolarity(string[i], string[i+1]):
+                # string = string[:i] + string[i+2:]
+                string = string.replace(string[i:i+2], '')
+                altered = True
+                break
+        if altered: continue
+        strLength = len(string.strip())
+        msg = f"Final polymer: {string}, length: {strLength}"
+        return msg
     pass
 
 def checkPolarity(unit1, unit2):
-    return False
+    return True if unit2 == matches[unit1] else False
 
-print(collapse_polymer("dabAcCaCBAcCcaDA")) # dabCBAcaDA
-
+# print(collapse_polymer("dabAcCaCBAcCcaDA")) # dabCBAcaDA
+print(collapse_polymer(fullInput))
+print(matches)
 
