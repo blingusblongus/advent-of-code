a7740d782c873a4367e37026ff1bf68ef69380aa
Nick Allen <rutherford.t.b@gmail.com>
2022-12-02 23:39:06 -0600

    complete 2022-3-1, 2022-3-2

---
+++ 2022/3/3-1.js
@@ -0,0 +1,32 @@
+import {test, input} from './input.js';
+
+
+const getPriority = (letter) => {
+    const order = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return order.indexOf(letter) + 1;
+}
+
+const solvePuzzle = (input) => {
+    let rucksacks = input.split(/\n+/g)
+        .map(sack => {
+            return [sack.slice(0, sack.length / 2), sack.slice(sack.length / 2)];
+        })
+
+    const findMatching = (rucksack) => {
+        const [first, second] = rucksack;
+
+        for(let x of first){
+            for(let y of second){
+                if(x === y){
+                    return x;
+                }
+            }
+        }
+    }
+
+    const doubles = rucksacks.map(sack => findMatching(sack));
+
+    return doubles.reduce((sum, el) => sum += getPriority(el), 0);
+}
+
+console.log(solvePuzzle(input))
